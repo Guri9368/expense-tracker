f9c9d4fc0f4b368a71bda229ed463a716da2c5e6
Guri9368 <144131390+Guri9368@users.noreply.github.com>
2026-02-18 05:56:13 +0530

    fix: final sqlite setup for vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,10 +5,7 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
